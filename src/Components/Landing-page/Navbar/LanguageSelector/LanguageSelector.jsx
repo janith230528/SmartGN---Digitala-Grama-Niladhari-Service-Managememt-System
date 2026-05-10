0746162b9b34c409d5edfcb119c643f5e61a8f41
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-05-11 00:20:06 +0530

    update navbar

---
+++ src/Components/Landing-page/Navbar/LanguageSelector/LanguageSelector.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './LanguageSelector.css'
+function LanguageSelector() {
+  return (
+    <div className="language-selector">
+            <div className ="nav-link-container">
+              <a href="#services" className="nav-link">
+              <img src="language_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg" alt="languageicon" className="icons" />
+              <span>English</span>
+              <img src="keyboard_arrow_down_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg" alt="languageicon" className="icons" /></a>
+              </div> 
+    </div>
+  )
+}
+
+export default LanguageSelector
--- src/Components/Landing-page/Navbar/Navbar.jsx
+++ src/Components/Landing-page/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import Logo from './Logo/Logo';
 import NavTabs from './NavTabs/NavTabs';
+import LanguageSelector from './LanguageSelector/LanguageSelector';
 
 
 function Navbar() {
@@ -10,16 +11,7 @@ function Navbar() {
       <div className="nav-container"> 
          <Logo />
          <NavTabs />
-      
-
-         <div className="language-selector">
-            <div className ="nav-link-container">
-              <a href="#services" className="nav-link">
-              <img src="language_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg" alt="languageicon" className="icons" />
-              <span>English</span>
-              <img src="keyboard_arrow_down_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg" alt="languageicon" className="icons" /></a>
-              </div> 
-          </div>
+         <LanguageSelector />
       </div>
 </nav>
   )
