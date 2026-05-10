3bc418aea7e280a3faba14a498c44716b02c01ee
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-05-10 23:44:03 +0530

    update Navbar folder structure

---
+++ src/Components/Landing-page/Navbar/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Logo.css'
+import logoImage from '../../../../assets/SmartGN logo/3.png';
+
+function Logo() {
+  return (
+    <div className="logo-section">
+          <img src={logoImage} alt="SmartGN Logo" className="logo" />
+    </div>
+    )
+}
+
+export default Logo
