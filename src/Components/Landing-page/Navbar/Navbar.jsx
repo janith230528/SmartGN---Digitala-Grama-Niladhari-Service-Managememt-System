e6b6ae76276bec164af9e9c62356865ddc53d5e0
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-05-11 12:02:07 +0530

    import tailwind css

--- src/Components/Landing-page/Navbar/Navbar.jsx
+++ src/Components/Landing-page/Navbar/Navbar.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import './Navbar.css'
 import Logo from './Logo/Logo';
 import NavTabs from './NavTabs/NavTabs';
 import LanguageSelector from './LanguageSelector/LanguageSelector';
-
+import './Navbar.css';
 
 function Navbar() {
-  return (
-    <nav className="navbar"> 
-      <div className="nav-container"> 
+return (
+  <nav className="w-full h-[100px] bg-[#EBF8FF] shadow-md sticky top-0 z-[1000]">
+      <div className="max-w-[1440px] h-full mx-auto px-[40px] flex justify-between items-center">
          <Logo />
          <NavTabs />
          <LanguageSelector />
       </div>
-</nav>
+  </nav>
   )
 }
 
